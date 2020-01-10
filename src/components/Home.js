a4422b5a79985b9d214cab7e56fce4ba6741c811
fatima ahmed <fatymah001@gmail.com>
2020-01-10 23:58:19 +0300

    Add about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './style.css';
+import image from '../main.jpeg';
 
 function Home(){
   return(
     <div id="home" className="home">
-      Home
+    <img className="main-img" src={image} alt="home"></img>
+    <div className="desc">
+      <h2>Hello, I'm Fatima Ahmed.</h2>
+      <br/>
+      <h2>I'm a full-stack web developer</h2>
+    </div>
     </div>
   )
 }
