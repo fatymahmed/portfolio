9da1558bc93d8feb78307454201201717dfa0106
fatima ahmed <fatymah001@gmail.com>
2020-01-10 23:10:02 +0300

    Add about and portfolio sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import image from '../my.png';
+import image from '../temp.jpeg';
 
 function About(){
   return(
-    <div id="about" className="About">
-      About
-      <img src={image} alt="comp"></img>
+    <div id="about" className="about container-fluid">
+      <div className="row">
+        <div className="me col-md-6 col-xs-12">
+          <img src={image} alt="comp"></img>
+          <h4>Who am I?</h4>
+          <p>A full stack developer.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident molestias nam laborum. Veniam esse dolore, non, a et vero ab doloremque fugiat voluptas ad magni labore rem perspiciatis placeat ipsam.</p>
+        </div>
+        <div className="col-md-6 col-xs-12 skills">
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"80%"}}>
+              <div className="language">HTML</div>
+            </div>
+            <span>80%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"70%"}}>
+              <div className="language">CSS</div>
+            </div>
+            <span>70%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"60%"}}>
+              <div className="language">Ruby/Rails</div>
+            </div>
+            <span>60%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"40%"}}>
+              <div className="language">React</div>
+            </div>
+            <span>40%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"50%"}}>
+              <div className="language">JavaScript</div>
+            </div>
+            <span>50%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-percentage" style={{width:"40%"}}>
+              <div className="language">Redux</div>
+            </div>
+            <span>40%</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,110 @@
 import React from 'react';
+import image from '../temp.jpeg';
 
 function Portfolio(){
+  function filterSelection(e) {
+    var c=e.target.id;
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c === "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+  }
+
+  // Show filtered elements
+  function AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) === -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+
+  // Hide elements that are not selected
+  function RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
   return(
     <div id="portfolio" className="Portfolio">
-      Portfolio
-    </div>
+      <div id="myBtnContainer">
+        <button className="btn active" id="all" onClick={filterSelection}> ALL</button>
+        <button className="btn" id="react" onClick={filterSelection}> REACT</button>
+        <button className="btn" id="ruby/rails" onClick={filterSelection}> RUBY/RAILS</button>
+        <button className="btn" id="javascript" onClick={filterSelection}> JAVASCRIPT</button>
+        <button className="btn" id="html/css" onClick={filterSelection}> HTML/CSS</button>
+      </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="filterDiv show col-md-4 col-sm-6 col-12">
+              <div class="box">
+                <img src={image} alt="comp"></img>
+                  <ul class="social-links">
+                    <li className="project-name"><h1>Facebook</h1></li>
+                    <br/>
+                    <li className='project-language'><h3>ruby/rails</h3></li>
+                    <br/>
+                    <li><a href="#"><i class="fa fa-code" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-desktop" aria-hidden="true"></i></a></li>
+                  </ul>
+              </div>
+            </div>
+            <div class="filterDiv show ruby/rails react col-md-4 col-sm-6 col-12">
+              <div class="box">
+                <img src={image} alt="comp"></img>
+                  <ul class="social-links">
+                    <li className="project-name"><h1>Bookstore</h1></li>
+                    <br/>
+                    <li className='project-language'><h3>React & Ruby/Rails</h3></li>
+                    <br/>
+                    <li><a href="#"><i class="fa fa-code" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-desktop" aria-hidden="true"></i></a></li>
+                  </ul>
+              </div>
+            </div>
+            <div class="filterDiv show react col-md-4 col-sm-6">
+              <div class="box">
+                <img src={image} alt="comp"></img>
+                  <ul class="social-links">
+                    <li className="project-name"><h1>Calculator</h1></li>
+                    <br/>
+                    <li className='project-language'><h3>ReactJs</h3></li>
+                    <br/>
+                    <li><a href="#"><i class="fa fa-code" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-desktop" aria-hidden="true"></i></a></li>
+                  </ul>
+              </div>
+            </div>
+            <div class="filterDiv show ruby/rails react col-md-4 col-sm-6">
+              <div class="box">
+                <img src={image} alt="comp"></img>
+                  <ul class="social-links">
+                    <li className="project-name"><h1>Scheduled Talks</h1></li>
+                    <br/>
+                    <li className='project-language'><h3>React & Ruby/Rails</h3></li>
+                    <br/>
+                    <li><a href="#"><i class="fa fa-code" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-desktop" aria-hidden="true"></i></a></li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
   )
 }
 
