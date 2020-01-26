e9e9f663ea90c4d807709b98a4106440e467e158
fatima ahmed <fatymah001@gmail.com>
2020-01-26 12:33:40 +0300

    Add style to filter bar in portfolio section

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,14 +7,14 @@ function Blog(){
     <div id="blog" className="Blogs container">
       <h1>BLOG</h1>
       <div className="row">
-        <div className="blog w3-animate-left col-md-4 col-sm-6 col-xs-12">
+        <div className="blog w3-animate-left col-md-6 col-sm-12 col-lg-4">
           <a href="https://hackernoon.com/professional-debugging-in-rails-1yr2bnz"> 
             <img className="article-img" src={image} alt="article"></img>
             <h5>Professional Debugging in Rails</h5>
             <p>Debugging is as important as writing code, it can cause your program to crash or behave unexpectedly. Most of the time, </p>
           </a>
         </div>
-        <div className="blog w3-animate-left col-md-4 col-sm-6 col-xs-12">
+        <div className="blog w3-animate-left col-md-6 col-sm-12 col-lg-4">
           <a href="https://medium.com/@fatima.ahmed.muhsin/how-to-use-a-css-background-gradient-56e1ab18a2ad"> 
             <img className="article-img" src={article2} alt="article"></img>
             <h5>How to use a CSS gradient background</h5>
